0c4ae202c115bc2a62b35ddac8a5ded55de67fd3
Eddie <elguardianenelsendero@hotmail.com>
2016-10-07 22:46:39 -0700

    Initial checkin

---
+++ app/src/main/java/mx/foodstack/chengzi/SettingsActivity.java
@@ -0,0 +1,14 @@
+package mx.foodstack.chengzi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by eecheverria on 10/7/2016.
+ */
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
